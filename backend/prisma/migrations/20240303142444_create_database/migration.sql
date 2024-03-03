62e847363cffd7530f49f8c5fb158c01454e7952
Ariel Narciso <arielnarciso@gmail.com>
2024-03-03 11:35:20 -0300

    fix: prisma migrate

--- backend/prisma/migrations/20240303142444_create_database/migration.sql
+++ backend/prisma/migrations/20240303142444_create_database/migration.sql
@@ -1,26 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Note` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Order` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Stock` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "Note";
-PRAGMA foreign_keys=on;
-
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "Order";
-PRAGMA foreign_keys=on;
-
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "Stock";
-PRAGMA foreign_keys=on;
-
 -- CreateTable
 CREATE TABLE "stock" (
     "id" TEXT NOT NULL PRIMARY KEY,
